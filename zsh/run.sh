39a1d4e3fb21a5f2e5a31742e256827fa985b1f9
CodnameJ <whjoon0225@naver.com>
2022-03-27 22:14:40 +0900

    Added zsh

---
+++ zsh/run.sh
@@ -0,0 +1,16 @@
+# Oh-My-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+# Powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+# zsh-z
+git clone https://github.com/agkozak/zsh-z $ZSH_CUSTOM/plugins/zsh-z
+# zsh-syntax-highlighting
+git clone https://github.co/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlightingm
+# zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+# change default shell
+chsh -s $(which zsh)
+# link the config file
+HERE=$(dirname $(realpath $0))
+rm ~/.zshrc
+ln -s $HERE/.zshrc ~/.zshrc
