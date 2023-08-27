f1c66cf627c91e072882c810f827f8def9fe422b
huGgW <whjoon0225@naver.com>
2023-08-27 09:12:36 +0900

    Change default colorscheme to everforest dark

--- nvim_lua/lua/configs/colorscheme.lua
+++ nvim_lua/lua/configs/colorscheme.lua
@@ -1,1 +1,12 @@
-vim.cmd[[colorscheme kanagawa]]
+
+vim.cmd([[
+    if has('termguicolors')
+      set termguicolors
+    endif
+    ]])
+
+vim.cmd([[let g:everforest_background = 'hard']])
+
+vim.cmd([[let g:everforest_better_performance = 1]])
+
+vim.cmd[[colorscheme everforest]]
