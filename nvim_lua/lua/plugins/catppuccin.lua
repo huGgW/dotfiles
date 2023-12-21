91a1092ca459b595804f5834060b2d7045dd1657
huGgW <whjoon0225@naver.com>
2023-12-22 02:39:10 +0900

    Set vim background opaque

--- nvim_lua/lua/plugins/catppuccin.lua
+++ nvim_lua/lua/plugins/catppuccin.lua
@@ -2,9 +2,9 @@ require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
     background = {     -- :h background
         light = "latte",
-        dark = "mocha",
+        dark = "macchiato",
     },
-    transparent_background = true, -- disables setting the background color.
+    transparent_background = false, -- disables setting the background color.
     show_end_of_buffer = false,    -- shows the '~' characters after the end of buffers
     term_colors = true,            -- sets terminal colors (e.g. `g:terminal_color_0`)
     dim_inactive = {
