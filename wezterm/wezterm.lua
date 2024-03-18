a607f0c954bb51f54b3bc8fb9a430a764f045656
huGgW <whjoon0225@naver.com>
2024-03-18 14:17:41 +0900

    Reformat wezterm config, change transparency and blur

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
--- This table will hold the configuration.
+------ This table will hold the configuration.
 local config = {}
 
 -- In newer versions of wezterm, use the config_builder which will
@@ -9,6 +9,11 @@ local config = {}
 if wezterm.config_builder then
   config = wezterm.config_builder()
 end
+
+
+------ This is where you actually apply your config choices
+
+---- color scheme settings
 
 -- wezterm.gui is not available to the mux server, so take care to
 -- do something reasonable when this config is evaluated by the mux
@@ -19,6 +24,7 @@ local function get_appearance()
     return 'Dark'
   end
 
+  -- get dark or light mode if supported
   if wezterm.gui then
     return wezterm.gui.get_appearance()
   end
@@ -34,31 +40,32 @@ local function scheme_for_appearance(appearance)
   end
 end
 
--- This is where you actually apply your config choices
 config.color_scheme = scheme_for_appearance(get_appearance())
 
+---- Font settings
 config.font = wezterm.font_with_fallback {
   {
     family = 'Monaspace Neon',
     harfbuzz_features = {'calt=1', 'liga=1', 'dlig=1'}
   },
-  -- {
-  --   family = 'JetBrainsMono Nerd Font',
-  --   harfbuzz_features = {'calt=1', 'liga=1', 'dlig=1'}
-  -- },
   {
-    family = 'D2Codingligature Nerd Font',
+    family = 'D2CodingLigature Nerd Font',
     harfbuzz_features = {'calt=1', 'liga=1', 'dlig=1'}
   }
 }
-
 config.font_size = 16
 
+---- Appearance settings
 config.hide_tab_bar_if_only_one_tab = true
-config.window_background_opacity = 0.95
-config.macos_window_background_blur = 16
+config.window_background_opacity = 0.825
+
+-- Macos blur
+config.macos_window_background_blur = 64
+
+-- Windows Mica 
 config.win32_system_backdrop = "Mica"
 
+-- FPS
 config.max_fps = 120
 
 
