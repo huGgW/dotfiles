14826f89a1a98c3e2acfb77da5e880ecee904ad5
logan <logan@channel.io>
2024-08-23 23:41:53 +0900

    wezterm - change to borderless transparent

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -56,16 +56,16 @@ config.font = wezterm.font_with_fallback {
 config.font_size = 13
 
 ---- Appearance settings
-config.hide_tab_bar_if_only_one_tab = true 
--- config.window_decorations = "RESIZE"
+config.hide_tab_bar_if_only_one_tab = true
+config.window_decorations = "RESIZE"
 
 -- blur and transparent
--- config.window_background_opacity = 0.8
--- config.macos_window_background_blur = 16
+config.window_background_opacity = 0.66
+config.macos_window_background_blur = 64
 
 -- solid
-config.window_background_opacity = 1
-config.macos_window_background_blur = 0
+-- config.window_background_opacity = 1
+-- config.macos_window_background_blur = 0
 
 
 -- Windows Mica 
