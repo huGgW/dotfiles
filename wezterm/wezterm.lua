b3ab8231d6b523e8978e2f838747dd91d1565448
logan <logan@channel.io>
2024-09-21 00:01:12 +0900

    wezterm - reduce transparency to 0.9

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -60,7 +60,7 @@ config.hide_tab_bar_if_only_one_tab = true
 config.window_decorations = "RESIZE"
 
 -- blur and transparent
-config.window_background_opacity = 0.66
+config.window_background_opacity = 0.90
 config.macos_window_background_blur = 64
 
 -- solid
