32a8ecdd11e64960a2f680e03b53c5c21954be26
huGgW <whjoon0225@naver.com>
2025-01-23 01:05:54 +0900

    zsh - add rancher desktop, remove rocm alias for linux

--- zsh/zshrc/linux.sh
+++ zsh/zshrc/linux.sh
@@ -13,16 +13,18 @@ fi
 # for amd MKL performance hack
 export MKL_DEBUG_CPU_TYPE=5
 
-# >>> ROCM docker >>>
-# alias for docker rocm
-alias drun='sudo docker run -it --network=host --device=/dev/kfd --device=/dev/dri --group-add=video --ipc=host --cap-add=SYS_PTRACE --security-opt seccomp=unconfined -e HSA_OVERRIDE_GFX_VERSION=10.3.0'
+# >>> ROCM >>>
 export HSA_OVERRIDE_GFX_VERSION=10.3.0
 export AMDGPU_TARGETS="gfx1030"
-# <<< ROCM docker <<<
+# <<< ROCM <<<
 
 # alias for enable & disable sleep in systemctl linux
 alias sleep_status="systemctl status sleep.target suspend.target hibernate.target hybrid-sleep.target"
 alias sleep_disable="systemctl mask sleep.target suspend.target hibernate.target hybrid-sleep.target"
 alias sleep_enable="systemctl unmask sleep.target suspend.target hibernate.target hybrid-sleep.target"
+
+# >>> Rancher Desktop >>>
+export PATH="$HOME/.rd/bin:$PATH"
+# <<< Rancher Desktop <<<
 
 # <<< Linux Specific <<<
