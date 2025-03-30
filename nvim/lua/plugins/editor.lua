946164661b348d2803dacfc9fbcc79e3e54ad3ed
huGgW <whjoon0225@naver.com>
2025-03-30 22:36:59 +0900

    nvim - add blink cmp

---
+++ nvim/lua/plugins/editor.lua
@@ -0,0 +1,17 @@
+return {
+    {
+        "saghen/blink.cmp",
+        dependencies = {
+            "rafamadriz/friendly-snippets",
+        },
+        version = "1.*",
+        opts = {
+            keymap = { preset = "enter" },
+            completion = {
+                documentation = {
+                    auto_show = true,
+                },
+            },
+        }
+    },
+}
