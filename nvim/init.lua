f650560b71e179b822360fe5a733fa75bfb82313
huGgW <whjoon0225@naver.com>
2025-03-30 21:30:56 +0900

    nvim - add rose-pine color scheme

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,5 @@
 -- Load lazy.nvim for plugin management
 require('config.lazy')
+
+-- Load colorscheme config
+require('config.colorscheme')
---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,13 @@
+return {
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",
+        priority = 999,
+        config = function()
+            require("rose-pine").setup({
+                variant = "auto",
+                dark_variant = "moon"
+            })
+        end,
+    },
+}
