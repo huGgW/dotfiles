92c4c05489f67a9f9df7f4f32137fbb74521579d
huGgW <whjoon0225@naver.com>
2025-03-30 22:36:50 +0900

    nvim - enable lsp, add go, lua config

--- nvim/init.lua
+++ nvim/init.lua
@@ -6,3 +6,13 @@ require('config.lazy')
 
 -- Load colorscheme config
 require('config.colorscheme')
+
+-- Enable LSPs on /lsp/*
+local configs = {}
+
+for _, v in ipairs(vim.api.nvim_get_runtime_file('lsp/*', true)) do
+    local name = vim.fn.fnamemodify(v, ':t:r')
+    configs[name] = true
+end
+
+vim.lsp.enable(vim.tbl_keys(configs))
