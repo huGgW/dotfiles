617f6cfaf80d529f5c9af623bf26f64550ff2391
huGgW <whjoon0225@naver.com>
2025-04-02 00:33:19 +0900

    nvim - configure snacks (without picker)

---
+++ nvim/lua/plugins/utility.lua
@@ -0,0 +1,43 @@
+return {
+    {
+        "folke/snacks.nvim",
+        priority = 998,
+        lazy = false,
+        opts = {
+            -- file utililty
+            bigfile = { enabled = true },
+            quickfile = { enabled = true },
+            -- TODO: config rename if needed
+
+            -- ui utility
+            indent = { enabled = true },
+            notifier = { enabled = true },
+            scroll = { enabled = true },
+            statuscolumn = { enabled = true }, -- FIXME: maybe should install gitsigns.nvim
+
+            -- image utility
+            image = { enabled = true },
+
+            -- git utility
+            lazygit = { enabled = true },
+
+            -- search utility
+            -- TODO: configure picker
+
+            -- extra utility
+            terminal = { enabled = true },
+            -- TODO: configure scratch if needed
+        },
+        keys = {
+            { "<leader>lg", function() Snacks.lazygit() end, desc = "LazyGit" },
+            { "<leader>tm", function() Snacks.terminal() end, desc = "Terminal" },
+        },
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+    },
+    {
+        'echasnovski/mini.nvim',
+        version = false,
+    },
+}
