94a73fad2aa068183ae7de4821947954a1d034b6
huGgW <whjoon0225@naver.com>
2025-04-03 01:37:21 +0900

    nvim - add nvim surround

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -9,4 +9,12 @@ return {
 	{
 		"christoomey/vim-tmux-navigator",
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
 }
