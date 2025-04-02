4d0d912063844f389506ab43dc68de99d689d828
huGgW <whjoon0225@naver.com>
2025-04-03 01:42:20 +0900

    nvim - add key picker config go snacks

--- nvim/lua/plugins/utility.lua
+++ nvim/lua/plugins/utility.lua
@@ -1,69 +1,157 @@
 return {
-    {
-        "folke/snacks.nvim",
-        priority = 998,
-        lazy = false,
-        opts = {
-            -- file utililty
-            bigfile = { enabled = true },
-            quickfile = { enabled = true },
-            explorer = { enabled = true },
-            -- TODO: config rename if needed
+	{
+		"folke/snacks.nvim",
+		priority = 998,
+		lazy = false,
+		opts = {
+			-- file utililty
+			bigfile = { enabled = true },
+			quickfile = { enabled = true },
+			explorer = { enabled = true },
+			-- TODO: config rename if needed
 
-            -- ui utility
-            indent = { enabled = true },
-            notifier = { enabled = true },
-            statuscolumn = { enabled = true },
-            -- scroll = { enabled = true },
+			-- ui utility
+			indent = { enabled = true },
+			notifier = { enabled = true },
+			statuscolumn = { enabled = true },
+			-- scroll = { enabled = true },
 
-            -- image utility
-            image = { enabled = true },
+			-- image utility
+			image = { enabled = true },
 
-            -- git utility
-            lazygit = { enabled = true },
+			-- git utility
+			lazygit = { enabled = true },
 
-            -- search utility
-            -- TODO: configure picker
+			-- search utility
+			-- TODO: configure picker
 
-            -- extra utility
-            terminal = { enabled = true },
-            -- TODO: configure scratch if needed
-        },
-        keys = {
-            { "<leader>lg", function() Snacks.lazygit() end, desc = "LazyGit" },
-            { "<C-`>", function() Snacks.terminal() end, mode = {"n", "t"}, desc = "Terminal" },
-            { "<leader>te", function() Snacks.explorer() end, desc = "Explorer" },
+			-- extra utility
+			terminal = { enabled = true },
+			-- TODO: configure scratch if needed
+		},
+		keys = {
+			{
+				"<leader>lg",
+				function()
+					Snacks.lazygit()
+				end,
+				desc = "LazyGit",
+			},
+			{
+				"<C-`>",
+				function()
+					Snacks.terminal()
+				end,
+				mode = { "n", "t" },
+				desc = "Terminal",
+			},
+			{
+				"<leader>te",
+				function()
+					Snacks.explorer()
+				end,
+				desc = "Explorer",
+			},
 
-            -- pickers
-            { "<leader>fo", function() Snacks.picker.files() end, desc = "Find Files" },
-            { "<leader>fs", function() Snacks.picker.grep() end, desc = "Grep" },
-            { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
-            { "<leader>fp", function() Snacks.picker.commands() end, desc = "Commands" },
-            -- pickers-LSP
-            { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
-            { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
-            { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
-            { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
-            { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
-            { "<leader>fd", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
-        },
-        dependencies = {
-            "nvim-tree/nvim-web-devicons",
-        },
-    },
-    {
-        "echasnovski/mini.nvim",
-        version = false,
-        config = function()
-            -- Text Editing
-            require("mini.pairs").setup()
+			-- pickers
+			{
+				"<leader>fo",
+				function()
+					Snacks.picker.files()
+				end,
+				desc = "Find Files",
+			},
+			{
+				"<leader>fs",
+				function()
+					Snacks.picker.grep()
+				end,
+				desc = "Grep",
+			},
+			{
+				"<leader>fb",
+				function()
+					Snacks.picker.buffers()
+				end,
+				desc = "Buffers",
+			},
+			{
+				"<leader>fp",
+				function()
+					Snacks.picker.commands()
+				end,
+				desc = "Commands",
+			},
+			{
+				"<leader>g?",
+				function()
+					Snacks.picker.keymaps()
+				end,
+				desc = "Keymaps",
+			},
 
-            -- General Workflow
+			-- LSP-related picker
+			{
+				"gd",
+				function()
+					Snacks.picker.lsp_definitions()
+				end,
+				desc = "Goto Definition",
+			},
+			{
+				"gD",
+				function()
+					Snacks.picker.lsp_declarations()
+				end,
+				desc = "Goto Declaration",
+			},
+			{
+				"gr",
+				function()
+					Snacks.picker.lsp_references()
+				end,
+				nowait = true,
+				desc = "References",
+			},
+			{
+				"gI",
+				function()
+					Snacks.picker.lsp_implementations()
+				end,
+				desc = "Goto Implementation",
+			},
+			{
+				"gy",
+				function()
+					Snacks.picker.lsp_type_definitions()
+				end,
+				desc = "Goto T[y]pe Definition",
+			},
+			{
+				"<leader>fd",
+				function()
+					Snacks.picker.lsp_workspace_symbols()
+				end,
+				desc = "LSP Workspace Symbols",
+			},
+		},
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+		},
+	},
+	{
+		"echasnovski/mini.nvim",
+		version = false,
+		config = function()
+			-- Text Editing
+			require("mini.pairs").setup()
 
-            -- Appearance
-            require("mini.animate").setup()
-            require("mini.statusline").setup()
-            require("mini.tabline").setup()
-        end,
-    },
+			-- General Workflow
+
+			-- Appearance
+			require("mini.animate").setup()
+			require("mini.statusline").setup()
+			require("mini.tabline").setup()
+		end,
+	},
 }
