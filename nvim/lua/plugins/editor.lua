e2f0a4e75c7c5748e46b0846fcc6b47f7238ec8a
huGgW <whjoon0225@naver.com>
2025-04-03 01:42:11 +0900

    nvim - add which-key

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -17,4 +17,18 @@ return {
 			require("nvim-surround").setup({})
 		end,
 	},
+	{
+		"folke/which-key.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{
+				"<leader>?",
+				function()
+					require("which-key").show({ global = false })
+				end,
+				desc = "Buffer Local Keymaps (which-key)",
+			},
+		},
+	},
 }
