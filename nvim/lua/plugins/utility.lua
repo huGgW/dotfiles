a5a94d6806f04f1fa906e71dc5e4d3dc1db02c6e
huGgW <whjoon0225@naver.com>
2025-04-02 23:55:13 +0900

    nvim - add pickers config

--- nvim/lua/plugins/utility.lua
+++ nvim/lua/plugins/utility.lua
@@ -33,6 +33,19 @@ return {
             { "<leader>lg", function() Snacks.lazygit() end, desc = "LazyGit" },
             { "<C-`>", function() Snacks.terminal() end, mode = {"n", "t"}, desc = "Terminal" },
             { "<leader>te", function() Snacks.explorer() end, desc = "Explorer" },
+
+            -- pickers
+            { "<leader>fo", function() Snacks.picker.files() end, desc = "Find Files" },
+            { "<leader>fs", function() Snacks.picker.grep() end, desc = "Grep" },
+            { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
+            { "<leader>fp", function() Snacks.picker.commands() end, desc = "Commands" },
+            -- pickers-LSP
+            { "gd", function() Snacks.picker.lsp_definitions() end, desc = "Goto Definition" },
+            { "gD", function() Snacks.picker.lsp_declarations() end, desc = "Goto Declaration" },
+            { "gr", function() Snacks.picker.lsp_references() end, nowait = true, desc = "References" },
+            { "gI", function() Snacks.picker.lsp_implementations() end, desc = "Goto Implementation" },
+            { "gy", function() Snacks.picker.lsp_type_definitions() end, desc = "Goto T[y]pe Definition" },
+            { "<leader>fd", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
         },
         dependencies = {
             "nvim-tree/nvim-web-devicons",
