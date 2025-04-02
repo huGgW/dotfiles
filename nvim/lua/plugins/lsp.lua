317d1a465332c0e4e7042b08ca633ffa019fd8e4
huGgW <whjoon0225@naver.com>
2025-04-03 01:13:43 +0900

    nvim - add conform

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -5,4 +5,29 @@ return {
             require("mason").setup()
         end
     }
+}	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		keys = {
+			{
+				"<leader>fm",
+				function()
+					require("conform").format({ async = true })
+				end,
+				mode = "",
+				desc = "Format buffer",
+			},
+		},
+		opts = {
+			formatters_by_ft = {
+				lua = { "stylua" },
+				go = { "gofumpt", "goimports" },
+			},
+			default_format_opts = {
+				lsp_format = "fallback",
+			},
+			format_on_save = { timeout_ms = 500 },
+		},
+	},
 }
