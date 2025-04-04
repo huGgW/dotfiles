5ea366328d2dd372ad351c07ef658ff4e309f84a
huGgW <whjoon0225@naver.com>
2025-04-05 00:30:08 +0900

    nvim - enable hipatterns, jump2d, trailspaces for mini

--- nvim/lua/plugins/utility.lua
+++ nvim/lua/plugins/utility.lua
@@ -31,110 +31,24 @@ return {
             -- TODO: configure scratch if needed
         },
         keys = {
-            {
-                "<leader>lg",
-                function()
-                    Snacks.lazygit()
-                end,
-                desc = "LazyGit",
-            },
-            {
-                "<C-`>",
-                function()
-                    Snacks.terminal()
-                end,
-                mode = { "n", "t" },
-                desc = "Terminal",
-            },
-            {
-                "<leader>te",
-                function()
-                    Snacks.explorer()
-                end,
-                desc = "Explorer",
-            },
+            { "<leader>lg", function() Snacks.lazygit() end,                      desc = "LazyGit" },
+            { "<C-`>",      function() Snacks.terminal() end,                     mode = { "n", "t" },            desc = "Terminal" },
+            { "<leader>te", function() Snacks.explorer() end,                     desc = "Explorer" },
 
             -- pickers
-            {
-                "<leader>fo",
-                function()
-                    Snacks.picker.files()
-                end,
-                desc = "Find Files",
-            },
-            {
-                "<leader>fs",
-                function()
-                    Snacks.picker.grep()
-                end,
-                desc = "Grep",
-            },
-            {
-                "<leader>fb",
-                function()
-                    Snacks.picker.buffers()
-                end,
-                desc = "Buffers",
-            },
-            {
-                "<leader>fp",
-                function()
-                    Snacks.picker.commands()
-                end,
-                desc = "Commands",
-            },
-            {
-                "<leader>g?",
-                function()
-                    Snacks.picker.keymaps()
-                end,
-                desc = "Keymaps",
-            },
+            { "<leader>fo", function() Snacks.picker.files() end,                 desc = "Find Files" },
+            { "<leader>fs", function() Snacks.picker.grep() end,                  desc = "Grep" },
+            { "<leader>fb", function() Snacks.picker.buffers() end,               desc = "Buffers" },
+            { "<leader>fp", function() Snacks.picker.commands() end,              desc = "Commands" },
+            { "<leader>g?", function() Snacks.picker.keymaps() end,               desc = "Keymaps" },
 
             -- LSP-related picker
-            {
-                "gd",
-                function()
-                    Snacks.picker.lsp_definitions()
-                end,
-                desc = "Goto Definition",
-            },
-            {
-                "gD",
-                function()
-                    Snacks.picker.lsp_declarations()
-                end,
-                desc = "Goto Declaration",
-            },
-            {
-                "gr",
-                function()
-                    Snacks.picker.lsp_references()
-                end,
-                nowait = true,
-                desc = "References",
-            },
-            {
-                "gI",
-                function()
-                    Snacks.picker.lsp_implementations()
-                end,
-                desc = "Goto Implementation",
-            },
-            {
-                "gy",
-                function()
-                    Snacks.picker.lsp_type_definitions()
-                end,
-                desc = "Goto T[y]pe Definition",
-            },
-            {
-                "<leader>fd",
-                function()
-                    Snacks.picker.lsp_workspace_symbols()
-                end,
-                desc = "LSP Workspace Symbols",
-            },
+            { "gd",         function() Snacks.picker.lsp_definitions() end,       desc = "Goto Definition" },
+            { "gD",         function() Snacks.picker.lsp_declarations() end,      desc = "Goto Declaration" },
+            { "gr",         function() Snacks.picker.lsp_references() end,        nowait = true,                  desc = "References" },
+            { "gI",         function() Snacks.picker.lsp_implementations() end,   desc = "Goto Implementation" },
+            { "gy",         function() Snacks.picker.lsp_type_definitions() end,  desc = "Goto T[y]pe Definition" },
+            { "<leader>fd", function() Snacks.picker.lsp_workspace_symbols() end, desc = "LSP Workspace Symbols" },
         },
         dependencies = {
             "nvim-tree/nvim-web-devicons",
@@ -148,11 +62,13 @@ return {
             require("mini.pairs").setup()
 
             -- General Workflow
+            require("mini.jump2d").setup()
 
             -- Appearance
-            -- require("mini.animate").setup()
             require("mini.statusline").setup()
             require("mini.tabline").setup()
+            require("mini.hipatterns").setup()
+            require("mini.trailspace").setup()
         end,
     },
 }
