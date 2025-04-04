86dfa2bc7bae97b6650ca295686713e6e0557e5a
logan <logan@channel.io>
2025-04-04 23:42:32 +0900

    nvim - add keymap for lsp

--- nvim/lua/config/keymap.lua
+++ nvim/lua/config/keymap.lua
@@ -10,3 +10,7 @@ vim.api.nvim_set_keymap("n", "<leader>p", '"+p', { desc = "paste" })
 vim.api.nvim_set_keymap("n", "<leader>P", '"+P', { desc = "paset line" })
 vim.api.nvim_set_keymap("v", "<leader>p", '"+p', { desc = "paste" })
 vim.api.nvim_set_keymap("v", "<leader>P", '"+P', { desc = "paset line" })
+
+-- Set lsp keymap
+vim.keymap.set("n", "<F2>", vim.lsp.buf.rename, { desc = "rename" })
+vim.keymap.set("n", "<leader>a", vim.lsp.buf.code_action, { desc = "code action" })
