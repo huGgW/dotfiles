03cc117d3b7e50a354fdba5f6384c9a69a08bfe8
logan <logan@channel.io>
2025-04-12 22:06:24 +0900

    nvim - add markdown plugin

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,39 +1,47 @@
 return {
-	{
-		"lewis6991/gitsigns.nvim",
-		event = "BufRead",
-		config = function()
-			require("gitsigns").setup()
-		end,
-	},
-	{
-		"christoomey/vim-tmux-navigator",
-	},
-	{
-		"kylechui/nvim-surround",
-		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({})
-		end,
-	},
-	{
-		"mg979/vim-visual-multi",
-		branch = master,
-		lazy = false,
-	},
-	{
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {},
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-		},
-	},
+    {
+        "lewis6991/gitsigns.nvim",
+        event = "BufRead",
+        config = function()
+            require("gitsigns").setup()
+        end,
+    },
+    {
+        "christoomey/vim-tmux-navigator",
+    },
+    {
+        "kylechui/nvim-surround",
+        version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({})
+        end,
+    },
+    {
+        "mg979/vim-visual-multi",
+        branch = master,
+        lazy = false,
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {},
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
+    {
+        'MeanderingProgrammer/markdown.nvim',
+        ft = { "markdown" },
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        opts = {
+            file_types = { "markdown" },
+        },
+    },
 }
