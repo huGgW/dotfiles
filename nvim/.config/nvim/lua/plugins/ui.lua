1bfdf119727c2006aa97b7d98e0f71af11b626d8
logan <logan@channel.io>
2025-10-24 15:17:40 +0900

    nvim - change to monokai

--- nvim/.config/nvim/lua/plugins/ui.lua
+++ nvim/.config/nvim/lua/plugins/ui.lua
@@ -66,9 +66,35 @@ return {
                     which_key = true,
                 },
             })
-
-            -- setup must be called before loading
-            vim.cmd.colorscheme "catppuccin"
         end
+    },
+    {
+        "loctvl842/monokai-pro.nvim",
+        priority = 999,
+        config = function()
+            require("monokai-pro").setup({
+                transparent_background = false,
+
+                day_night = {
+                    enable = true,
+                    day_filter = "light",
+                    night_filter = "pro",
+                },
+
+                override = function(c)
+                    local hp = require("monokai-pro.color_helper")
+                    local common_fg = hp.lighten(c.sideBar.foreground, 30)
+                    return {
+                        SnacksPicker = { bg = c.editor.background, fg = common_fg },
+                        SnacksPickerBorder = { bg = c.editor.background, fg = c.tab.unfocusedActiveBorder },
+                        SnacksPickerTree = { fg = c.editorLineNumber.foreground },
+                        SnacksPickerDirectory = { fg = c.editorLineNumber.foreground },
+                        NonText = { fg = c.base.dimmed3 }, -- not sure if this should be broken into all hl groups importing NonText
+                    }
+                end,
+            })
+
+            vim.cmd.colorscheme("monokai-pro")
+        end,
     },
 }
