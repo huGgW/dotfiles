465da100e6f7ab3bc0be6822063e2425da93f086
huGgW <whjoon0225@naver.com>
2026-01-24 15:36:55 +0900

    zsh - add opam config

--- zsh/zshrc/tools.sh
+++ zsh/zshrc/tools.sh
@@ -136,3 +136,10 @@ if command -v mise > /dev/null 2>&1; then
     eval "$(mise activate zsh)"
 fi
 
+# opam configuration
+# This is useful if you're using opam as it adds:
+#   - the correct directories to the PATH
+#   - auto-completion for the opam binary
+# This section can be safely removed at any time if needed.
+[[ ! -r '~/.opam/opam-init/init.zsh' ]] || source '~/.opam/opam-init/init.zsh' > /dev/null 2> /dev/null
+# END opam configuration
